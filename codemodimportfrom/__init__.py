d6a4478b456d223094c4ccf57e5d57630a9c8f1b
Peter Byfield <peter.byfield@octoenergy.com>
2023-10-16 11:44:19 +0200

    Simplify by using QualifiedNameProvider

--- codemodimportfrom/__init__.py
+++ codemodimportfrom/__init__.py
@@ -5,18 +5,16 @@ from libcst import RemovalSentinel, FlattenSentinel  # TODO tidy
 
 
 class Transformer(libcst.CSTTransformer):
+    METADATA_DEPENDENCIES = (libcst.metadata.QualifiedNameProvider,)
+
     def __init__(self, importfrom: str):
         self.importfrom = importfrom
 
         self._imports_to_replace = set()
-        self._names_to_replace = set()
-        self._attributes = []
 
     def visit_ImportFrom(self, node: "ImportFrom") -> Optional[bool]:
         if node.module.value == self.importfrom:
             self._imports_to_replace.add(node)
-            for name in node.names:
-                self._names_to_replace.add(name.name.value)
         return False
 
     def leave_ImportFrom(
@@ -28,46 +26,33 @@ class Transformer(libcst.CSTTransformer):
             return libcst.Import(
                 names=[libcst.ImportAlias(name=libcst.Name(value=self.importfrom))]
             )
-        else:
-            return updated_node
-
-    def visit_Attribute(self, node: "Attribute") -> Optional[bool]:
-        self._attributes.append(node)
-
-    def leave_Attribute(
-        self, original_node: "Attribute", updated_node: "Attribute"
-    ) -> "BaseExpression":
-        self._attributes.pop()
         return updated_node
 
     def leave_Name(
         self, original_node: "Name", updated_node: "Name"
     ) -> "BaseExpression":
-        if original_node.value in self._names_to_replace:
-            name_to_replace = original_node.value
-            first_attribute = self._attributes[0].value if self._attributes else None
-            if (
-                not self._attributes
-                or (
-                    isinstance(first_attribute, libcst.Name)
-                    and first_attribute.value == name_to_replace
-                )
-                or (
-                    isinstance(first_attribute, libcst.Call)
-                    and isinstance(first_attribute.func, libcst.Name)
-                    and first_attribute.func.value == name_to_replace
-                )
-            ):
-                return libcst.Attribute(
-                    value=libcst.Name(value=self.importfrom),
-                    attr=libcst.Name(value=name_to_replace),
-                )
+        qualified_names = self.get_metadata(
+            libcst.metadata.QualifiedNameProvider, original_node
+        )
+        if len(qualified_names) == 0:
+            return updated_node
+        if len(qualified_names) > 1:
+            raise Exception  # TODO
+        qualified_name: libcst.metadata.QualifiedName = qualified_names.pop()
+
+        if (
+            qualified_name.source == libcst.metadata.QualifiedNameSource.IMPORT
+            and qualified_name.name == f"{self.importfrom}.{original_node.value}"
+        ):
+            return libcst.Attribute(
+                value=libcst.Name(value=self.importfrom),
+                attr=libcst.Name(value=original_node.value),
+            )
         return updated_node
 
 
 def transform_importfrom(*, code: str, importfrom: str) -> str:
     tree = libcst.parse_module(code)
-    # print(tree)  # TODO
-    transformer = Transformer(importfrom)
-    tree = tree.visit(transformer)
+    wrapper = libcst.metadata.MetadataWrapper(tree)
+    tree = wrapper.visit(Transformer(importfrom))
     return tree.code  # TODO black, optimize imports (isort/ruff)?
--- tests/test_codemodimportfrom.py
+++ tests/test_codemodimportfrom.py
@@ -1,48 +1,87 @@
+import pytest
+
 import codemodimportfrom
 
 
-def test_rewrites_imports():
-    code = """
+@pytest.mark.parametrize(
+    "code,expected_transformed_code",
+    [
+        [
+            """
 from foo import a, b
-from bar import c, d
-import bax
-
 a
-b()
-
-a.x
-b().y
-
-c
-d()
-
-bax.a
-bax.a(a)
-bax.a[0](a)
-""".strip()
+b()""",
+            """
+import foo
+foo.a
+foo.b()""",
+        ],
+        #
+        [
+            """
+from foo import a
+from bar import b
+a
+b""",
+            """
+import foo
+from bar import b
+foo.a
+b""",
+        ],
+        #
+        [
+            """
+from foo import a
+from bar import b
+a.a
+b.a""",
+            """
+import foo
+from bar import b
+foo.a.a
+b.a""",
+        ],
+        #
+        [
+            """
+from foo import a
+a
+def bar():
+    x = a.x
+    a = A()
+    b = a.b
+a""",
+            """
+import foo
+foo.a
+def bar():
+    x = foo.a.x
+    a = A()
+    b = a.b
+foo.a""",
+        ],
+        #
+        [
+            """
+from foo import a
+a
+class Foo:
+    a: int""",
+            """
+import foo
+foo.a
+class Foo:
+    a: int""",
+        ],
+    ],
+)
+def test_rewrites_imports(code, expected_transformed_code):
+    code = code.strip()
+    expected_transformed_code = expected_transformed_code.strip()
 
     transformed_code = codemodimportfrom.transform_importfrom(
         code=code, importfrom="foo"
     )
 
-    assert (
-        transformed_code
-        == """
-import foo
-from bar import c, d
-import bax
-
-foo.a
-foo.b()
-
-foo.a.x
-foo.b().y
-
-c
-d()
-
-bax.a
-bax.a(foo.a)
-bax.a[0](foo.a)
-""".strip()
-    )
+    assert transformed_code == expected_transformed_code
